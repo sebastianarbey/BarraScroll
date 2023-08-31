59dedfec7c9f8915d911bf74832f4a9d87a55d02
Glitch (glitch-hello-website) <none>
2023-08-31 14:26:59 +0000

    💐📷 Checkpoint ./style.css:57384704/104 ./script.js:57384704/663

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
-window.addEventListener('load', () =>{
-const progress = document.getElementById('progress');
-requestAnimationFrame(update);
-})
 
-function update() {
- progress.style.width = `${((window.scrollY) / (document.body.scrollHeight - window.innerHeigth) * 100)}%`; 
-requestAnimationFrame(update);
-}
+
+window.addEventListener('load', () => {
+    const progress = document.getElementById('progress');
+    /* console.log(progress); */
+    /* update(); */
+    requestAnimationFrame(update);
+
+  });
+  
+  function update() {
+    progress.style.width = `${((window.scrollY) / (document.body.scrollHeight - window.innerHeight) * 100)}%`
+    requestAnimationFrame(update);
+  }
